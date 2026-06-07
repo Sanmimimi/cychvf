74163f403312ae0e20d7410499fa26061eaf5bdf
Sanmimimi <3864620423@qq.com>
2026-06-07 22:15:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,64 +1,67 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import obfuscatorPlugin from 'vite-plugin-obfuscator'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    obfuscatorPlugin({
-      // 极度混淆配置
-      compact: true,
-      controlFlowFlattening: true,
-      controlFlowFlatteningThreshold: 1,
-      deadCodeInjection: true,
-      deadCodeInjectionThreshold: 1,
-      debugProtection: true,
-      debugProtectionInterval: 4000,
-      disableConsoleOutput: true,
-      identifierNamesGenerator: 'hexadecimal',
-      log: false,
-      numbersToExpressions: true,
-      renameGlobals: false,
-      selfDefending: true,
-      simplify: true,
-      splitStrings: true,
-      splitStringsChunkLength: 5,
-      stringArray: true,
-      stringArrayCallsTransform: true,
-      stringArrayEncoding: ['rc4'],
-      stringArrayIndexShift: true,
-      stringArrayRotate: true,
-      stringArrayShuffle: true,
-      stringArrayWrappersCount: 5,
-      stringArrayWrappersChainedCalls: true,
-      stringArrayWrappersType: 'function',
-      stringArrayThreshold: 1,
-      transformObjectKeys: true,
-      unicodeEscapeSequence: true
-    })
+    vue()
   ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined,
         // 打包成一个文件
-        inlineDynamicImports: true
+        inlineDynamicImports: true,
+        // 手动分包，防止代码分割
+        manualChunks: undefined
       }
     },
     // 禁用 sourcemap
     sourcemap: false,
-    // 压缩
+    // 使用 terser 压缩
     minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
         drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug']
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn']
       },
       mangle: {
         toplevel: true,
-        safari10: true
+        safari10: true,
+        // 混淆属性名
+        properties: {
+          regex: /^_/
+        }
+      },
+      output: {
+        // 美化输
+        comments: false,
+        // 使用Unicode转义序列
+        ascii_only: true
+      },
+      // 开启名称混淆
+      keep_classnames: false,
+      keep_fnames: false,
+      toplevel: true
+    },
+    // 设置chunk大小警告限制
+    chunkSizeWarningLimit: 1000,
+    // 输出目录
+    outDir: 'dist',
+    // 静态资源目录
+    assetsDir: 'static',
+    // 打包后的文件名
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
+  },
+  // 定义全局常量
+  define: {
+    __VUE_OPTIONS_API__: false,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
   }
 })
